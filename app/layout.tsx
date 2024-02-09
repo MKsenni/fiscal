fba8161046f2e33590433797eb904b5572cdb9bc
Kseniya <pen.cil@mail.ru>
2024-02-09 15:14:33 +0500

    feat: add web analytic by vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from './components/header/header';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body className={inter.className + ' text-xs md:text-sm lg:text-base'}>
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   );
