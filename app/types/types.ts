8b8d7b760254ad80159a1c41d767cf1d2e58b1d8
Kseniya <pen.cil@mail.ru>
2024-02-27 00:38:32 +0500

    fix: change constants and types

--- app/types/types.ts
+++ app/types/types.ts
@@ -1,16 +1,16 @@
 export type ServicesPackagesType = {
-  subtitle: string,
-  description: string,
-  services: string[],
-}
+  subtitle: string;
+  description: string;
+  services: string[];
+};
 
 export type ServiceItemType = {
   name: string;
   price: string;
-}
+};
 
 export type ServicesType = {
-  ndfl: ServiceItemType[],
-  registration: ServiceItemType[],
-  etcServices: ServiceItemType[],
-} 
+  ndfl: ServiceItemType[];
+  registration: ServiceItemType[];
+  etcServices: ServiceItemType[];
+};
